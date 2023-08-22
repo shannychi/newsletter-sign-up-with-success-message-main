8c859f3f7825f05a551b7d17843e0a1a0e3c967a
Edeh sharon <Pearllizzy196@gmail.com>
2023-08-22 17:29:13 +0100

    added hover

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -11,3 +11,25 @@ function switchVisible() {
       }
   }
 }
+
+function valideForm() {
+  let estvalide = true;
+
+  const emailInput = document.getElementById("email");
+
+  if(emailInput.value === "") {
+    document.getElementById("error_message") .innerHTML = "vaild email required";
+    emailInput.nextElementSibling.style.display = "block";
+    emailInput.style.border = "1px solid hsl(4, 100%, 67%)";
+    emailInput.style.background = "hsl(3, 39%, 81%)";
+
+    estvalide = false;
+  } else {
+    emailInput.nextElementSibling.style.display = "none";
+    emailInput.style.border = "1px solide black";
+  }
+
+  if(estvalide) {
+    return switchVisible();
+  }
+}
