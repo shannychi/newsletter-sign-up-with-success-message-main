b3d5e74e6410328a7b3b9411137cbc7f417ece6a
Edeh sharon <Pearllizzy196@gmail.com>
2023-08-22 12:34:48 +0100

    added code to switch between the 2 cards

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,8 +1,13 @@
-const form = document.querySelector("form");
-const card_1 = document.querySelector(".container");
-const card_2 = document.querySelector(".row3");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  card_1.classList.add("hide");
-  card_2.classList.remove("hide");
-});
+function switchVisible() {
+  if (document.getElementById('a')) {
+
+      if (document.getElementById('a').style.display == 'none') {
+          document.getElementById('a').style.display = 'block';
+          document.getElementById('b').style.display = 'none';
+      }
+      else {
+          document.getElementById('a').style.display = 'none';
+          document.getElementById('b').style.display = 'block';
+      }
+  }
+}
