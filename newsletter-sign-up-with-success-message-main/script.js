3685a70c41f95f892c9b2710e1e081957675e645
Edeh sharon <Pearllizzy196@gmail.com>
2023-08-22 01:11:32 +0100

    added some code

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,12 +1,8 @@
-function email() {
-    var mail = document.getElementById("text").value;
-
-    var regx = /^([a-zA-z0-9\._]+)@([a-zA-z0-9])+.([a-z]+)(.[a-z])?$/
-        
-    if(regx.text(mail)) {
-return true;
-    } else {
-        alert("vaild email required");
-        return false;
-    }
-}
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".container");
+const card_2 = document.querySelector(".row3");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
